afe83b25bfebe51117a453b09e20d090f1a819c3
EDUARDO GABRIEL <programador.edu.dev@gmail.com>
2025-06-21 01:18:47 -0300

    Adicionado slide responsivo na página inicial

---
+++ script.js
@@ -0,0 +1,30 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+  }
+});
+
